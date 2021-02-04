cdf563a07899c2ebbd21626c5220d3e5b82f0ddf
sjsamphex <sjsamphex@gmail.com>
2021-02-04 11:13:00 -0800

    user model uses UUIDV4 as pk

--- server/db/index.js
+++ server/db/index.js
@@ -1,31 +1,35 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require('./db');
 
 const User = require('./models/user');
 
-//associations could go here!
+// //associations could go here!
+// Items.belongsToMany(Users, { through: ManyToManyTable });
+// Users.belongsToMany(Items, { through: ManyToManyTable });
 
-const syncAndSeed =  async()=> {
-  await db.sync({force: true})
+const syncAndSeed = async () => {
+  await db.sync({ force: true });
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
-  ])
+    User.create({ email: 'cody@email.com', password: '123' }),
+    User.create({ email: 'murphy@email.com', password: '123' }),
+  ]);
   const [cody, murphy] = users;
 
+  //create some items here
+  //place some items in user's carts
   return {
     users: {
       cody,
-      murphy
-    }
+      murphy,
+    },
   };
-}
+};
 
 module.exports = {
   db,
   syncAndSeed,
   models: {
-    User
-  }
-}
+    User,
+  },
+};
