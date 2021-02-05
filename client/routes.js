02e38476eb90936c2efe24ee309b165a87a3d5fa
kuperavv <vince.kuperavage@gmail.com>
2021-02-04 19:03:05 -0600

    adding singlePage route on client side

--- client/routes.js
+++ client/routes.js
@@ -1,7 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
-import { Login, Signup, Home, Account, ViewCart } from './components';
+import {
+  Login,
+  Signup,
+  Home,
+  Account,
+  ViewCart,
+  SingleItem,
+} from './components';
 import { me } from './store';
 
 /**
@@ -22,12 +29,14 @@ class Routes extends Component {
             <Route path="/home" component={Home} />
             <Route path="/account" component={Account} />
             <Route path="/viewcart" component={ViewCart} />
+            <Route path={`/singleItem/1`} component={SingleItem} />
             <Redirect to="/home" />
           </Switch>
         ) : (
           <Switch>
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
+            <Route path={`/singleItem/1`} component={SingleItem} />
             <Redirect to="/login" />
           </Switch>
         )}
@@ -44,6 +53,7 @@ const mapState = (state) => {
     // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
     // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
     isLoggedIn: !!state.auth.id,
+    singleItem: state.singleItem,
   };
 };
 
