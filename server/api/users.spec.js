9d559e553e96d499b575b7aca78e486a3846b304
sjsamphex <sjsamphex@gmail.com>
2021-02-13 08:22:12 -0800

    attempts at test scripts and implementation of auth in users.spec.js

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -1,24 +1,34 @@
 /* global describe beforeEach it */
 
-const {expect} = require('chai')
-const request = require('supertest')
-const { db, syncAndSeed, models: { User } } = require('../db')
-const app = require('../app')
+const { expect } = require('chai');
+const request = require('supertest');
+const {
+  db,
+  syncAndSeed,
+  models: { User },
+} = require('../db');
+const app = require('../app');
 
 describe('User routes', () => {
-  beforeEach(async() => {
+  beforeEach(async () => {
     await syncAndSeed();
-  })
+    const auth = (
+      await axios.post(`/auth/login`, { email: 'admin', password: 'admin' })
+    ).data;
+  });
 
   describe('/api/users/', () => {
-
     it('GET /api/users', async () => {
       const res = await request(app)
-        .get('/api/users')
-        .expect(200)
+        .get('/api/users', {
+          headers: {
+            authorization: token,
+          },
+        })
+        .expect(200);
 
       expect(res.body).to.be.an('array');
       expect(res.body.length).to.equal(2);
-    })
-  }) // end describe('/api/users')
-}) // end describe('User routes')
+    });
+  }); // end describe('/api/users')
+}); // end describe('User routes')
