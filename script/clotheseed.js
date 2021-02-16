b027398e7290e936e91a8d449456dcfae30c2e28
sjsamphex <sjsamphex@gmail.com>
2021-02-16 08:41:04 -0800

    moved images, front end client now can call up images

--- script/clotheseed.js
+++ script/clotheseed.js
@@ -1,28 +1,32 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-// let clothes = JSON.parse(fs.readFileSync('./script/clothes.json', 'utf8'));
 let clothes = JSON.parse(
   fs.readFileSync('./script/winter-clothes.json', 'utf8')
 );
 
 //https://www.kaggle.com/paramaggarwal/fashion-product-images-small
 const Item = require('../server/db/models/item');
+const faker = require('faker');
+
 module.exports = {
   async clotheseed() {
     console.log('seeding big ass json now');
-    // clothes = clothes.filter((clothe) => {
-    //   return clothe.season === 'Winter';
-    // });
-    // fs.writeFileSync('winter-clothes', JSON.stringify(clothes));
     console.log(clothes.length);
+
+    // const categories = ['Clothes', 'Gear', 'Accessories'];
 
     await Promise.all(
       clothes.map((clothe) =>
         Item.create({
-          imageURL: clothe.id,
+          imageURL: `/images/GWGImages/${clothe.id}.jpg`,
           name: clothe.productDisplayName,
           category: clothe.MasterCategory,
+          //     category: categories[i % 3],
+
+          description: faker.commerce.productDescription(),
+          price: faker.commerce.price(),
+          rating: Math.random() * 5,
         })
       )
     );
