d1b985159a89b0417fe76f93ed246178e5b80990
sjsamphex <sjsamphex@gmail.com>
2021-02-15 18:29:08 -0800

    smaller csv with only winter items

--- script/clotheseed.js
+++ script/clotheseed.js
@@ -1,13 +1,22 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const clothes = JSON.parse(fs.readFileSync('./script/clothes.json', 'utf8'));
+// let clothes = JSON.parse(fs.readFileSync('./script/clothes.json', 'utf8'));
+let clothes = JSON.parse(
+  fs.readFileSync('./script/winter-clothes.json', 'utf8')
+);
+
 //https://www.kaggle.com/paramaggarwal/fashion-product-images-small
 const Item = require('../server/db/models/item');
 module.exports = {
   async clotheseed() {
     console.log('seeding big ass json now');
+    // clothes = clothes.filter((clothe) => {
+    //   return clothe.season === 'Winter';
+    // });
+    // fs.writeFileSync('winter-clothes', JSON.stringify(clothes));
     console.log(clothes.length);
+
     await Promise.all(
       clothes.map((clothe) =>
         Item.create({
