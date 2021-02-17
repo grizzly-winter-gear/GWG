7aac8ae9b5d813063a7fe7f9995cc728ef6e143b
sjsamphex <sjsamphex@gmail.com>
2021-02-17 13:48:41 -0800

    convert stock alert into a dialog transition box upon attempting to check out too many items over stock limit

--- client/components/stripe.js
+++ client/components/stripe.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Grid from '@material-ui/core/Grid';
 import { loadStripe } from '@stripe/stripe-js';
+
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const Stripe_Publishable_Key =
   'pk_test_51IIkWkKeRKIJY4WC39oRsOkkq1KnvUKMqUR85SfUbKdifuDN3XqsGsUs2wIxApF9sr6ZdggRIIkb0pviH62g2dN000huT4rH5A';
@@ -13,8 +25,13 @@ class Stripe extends Component {
     super(props);
     this.state = {
       Quantity: [],
+      dialog: false,
     };
     this.handleClick = this.handleClick.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+  handleClose(event) {
+    this.setState({ dialog: false });
   }
   async handleClick(event) {
     // Get Stripe.js instance
@@ -56,18 +73,18 @@ class Stripe extends Component {
       // console.log(session.quantityCheck);
       this.setState({
         Quantity: session.quantityCheck,
+        dialog: true,
       });
     }
   }
 
   render() {
-    console.log(this.state.Quantity);
     return (
       <Grid>
-        <button role="link" onClick={() => this.handleClick()}>
+        <button role="link" onClick={this.handleClick}>
           Checkout
         </button>
-        {this.state.Quantity.length > 0 && (
+        {/* {this.state.Quantity.length > 0 && (
           <Grid item>
             There's a problem with this purchase:{' '}
             {this.state.Quantity.map((item) => (
@@ -76,7 +93,34 @@ class Stripe extends Component {
               </Grid>
             ))}
           </Grid>
-        )}
+        )} */}
+        <Dialog
+          open={this.state.dialog}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={this.handleClose}
+        >
+          <DialogTitle>{'Out of Stock'}</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              There's a problem with this purchase:{' '}
+              {this.state.Quantity.map((item) => (
+                <span key={item.itemId}>
+                  <br />
+                  <b>{item.name}</b> only has <b> {item.stock}</b> left in
+                  stock.
+                  <br />
+                </span>
+              ))}
+              Please adjust your cart quantities before checking out.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Grid>
     );
   }
