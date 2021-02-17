c39efb9cd7c4bc4916433dbe24e9322cb462db56
sjsamphex <sjsamphex@gmail.com>
2021-02-16 18:20:10 -0800

    itemSeed now will be fed into Item db

--- server/db/index.js
+++ server/db/index.js
@@ -66,44 +66,41 @@ const syncAndSeed = async () => {
   ];
 
   //create some items here
-  const items = await Promise.all([
-    Item.create({ name: 'helmet a ', category: 'Gear' }),
-    Item.create({ name: 'cool boots', category: 'Gear' }),
-  ]);
+  await Promise.all([itemSeed.map((item) => Item.create(item))]);
   const categories = ['Clothes', 'Gear', 'Accessories'];
-  for (let i = 0; i < 100; i++) {
-    await Item.create({
-      name: faker.commerce.productName(),
-      category: categories[i % 3],
-      description: faker.commerce.productDescription(),
-      price: faker.commerce.price(),
-      rating: Math.random() * 5,
-      imageURL: faker.image.imageUrl(null, null, 'sports', true),
-    });
-  }
-  const [helmet, boot] = items;
+  // for (let i = 0; i < 100; i++) {
+  //   await Item.create({
+  //     name: faker.commerce.productName(),
+  //     category: categories[i % 3],
+  //     description: faker.commerce.productDescription(),
+  //     price: faker.commerce.price(),
+  //     rating: Math.random() * 5,
+  //     imageURL: faker.image.imageUrl(null, null, 'sports', true),
+  //   });
+  // }
+  // const [helmet, boot] = items;
   // await cody.addItem(boot, { through: { status: 'unpurchased', quantity: 1 } });
   // await murphy.addItem(helmet, {
   //   through: { status: 'unpurchased', quantity: 1 },
   // });
-  const tempcart = await Cart.create({
-    status: 'unpurchased',
-    userId: murphy.id,
-  });
-  await tempcart.addItem(boot);
+  // const tempcart = await Cart.create({
+  //   status: 'unpurchased',
+  //   userId: murphy.id,
+  // });
+  // await tempcart.addItem(boot);
 
-  const tempcart2 = await Cart.create({
-    status: 'unpurchased',
-    userId: cody.id,
-  });
-  await tempcart2.addItem(helmet);
+  // const tempcart2 = await Cart.create({
+  //   status: 'unpurchased',
+  //   userId: cody.id,
+  // });
+  // await tempcart2.addItem(helmet);
 
   return {
     users: {
       cody,
       murphy,
     },
-    items,
+    // items,
   };
 };
 
